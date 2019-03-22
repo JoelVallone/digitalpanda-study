4cb630b7802047745cccb9a0ad7030baccd03a82
Joël Vallone <joel.vallone@gmail.com>
2019-03-22 14:57:43 +0100

    Found all bugs

--- scala-tutorial/functional_program_design_in_scala/w3/ex1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ scala-tutorial/functional_program_design_in_scala/w3/ex1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -7,6 +7,7 @@ import Gen._
 import Prop._
 
 import scala.math.min
+import scala.math.max
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
@@ -14,7 +15,7 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     const(empty),
     for {
       x <- arbitrary[A]
-      h <- oneOf[H](empty, genHeap)
+      h <- oneOf[H](genHeap, genHeap, genHeap, empty)
     } yield insert(x, h)
   )
 
@@ -32,47 +33,77 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
       findMin(h) == a
     }
 
-  property("find smaller among pair inserted in empty") =
-    forAll { (a : A, b: A)  =>
-      val h = insert(a, insert(b, empty))
-      findMin(h) == min(a, b)
+  def insertAll(a : List[A], h: H): H =
+    if (a.isEmpty) h else insertAll(a.tail, insert(a.head,h))
+
+  property("find smaller among list inserted in empty") =
+    forAll { l : List[A]  =>
+      if (l.isEmpty) true else findMin(insertAll(l,empty)) == l.min
     }
 
-  property("insert & delete into empty should lead empty") =
-    forAll { a: A =>
-      val h = insert(a, empty)
-      deleteMin(h) == empty
-    }
 
-  def toList(heap: H): List[A] = {
-    def toListPartial(partialList: List[A], heap: H) : List[A] = {
-      if (isEmpty(heap))
-        partialList
+  def deleteMinN(h: H, n: Int): H =
+    if (n == 0) h else deleteMinN(deleteMin(h), n-1)
+
+  property("insert & delete all into empty should lead empty") =
+    forAll { l : List[A]  =>
+      if (l.isEmpty)
+        true
       else {
-        val min = findMin(heap)
-        toListPartial(min :: partialList, deleteMin(heap))
+        val h = insertAll(l, empty)
+        empty == deleteMinN(h, l.length)
       }
-    }
-    toListPartial(Nil, heap)
-  }
-  property("empty a heap with min should lead a sorted list") =
-    forAll { h: H =>
-      val list = toList(h)
-      list.sorted == list.reverse
     }
 
-  property("meld into one should find min among former two heaps") =
-    forAll { (h2: H, h1: H) =>
-      if (isEmpty(h1)) {
-        if (isEmpty(h2))
-          true
-        else
-          findMin(meld(h1, h2)) == findMin(h2)
-      } else {
-        if (isEmpty(h2))
-          findMin(meld(h1, h2)) == findMin(h1)
-        else
-          findMin(meld(h1, h2)) == min(findMin(h2),findMin(h1))
+    def toList(heap: H): List[A] = {
+      def toListPartial(partialList: List[A], heap: H) : List[A] = {
+        if (isEmpty(heap))
+          partialList
+        else {
+          val min = findMin(heap)
+          toListPartial(min :: partialList, deleteMin(heap))
+        }
       }
+      toListPartial(Nil, heap)
+    }
+    property("empty a heap with min should lead a sorted list") =
+      forAll { h: H =>
+        val list = toList(h)
+        list.sorted == list.reverse
+      }
+
+    property("break5: meld into one should find min among former two heaps") =
+      forAll { (h2: H, h1: H) =>
+        if (isEmpty(h1)) {
+          if (isEmpty(h2))
+            true
+          else
+            findMin(meld(h1, h2)) == findMin(h2)
+        } else {
+          if (isEmpty(h2))
+            findMin(meld(h1, h2)) == findMin(h1)
+          else
+            findMin(meld(h1, h2)) == min(findMin(h2),findMin(h1))
+        }
+      }
+
+  property("break3:  meld max heap with min heap and delete min should only hold max") =
+    forAll { (a : A, b: A) =>
+        val h1 = insert(min(a,b), empty)
+        val h2 = insert(max(a,b), empty)
+        val h21 = meld(h2, h1)
+        val h = deleteMin(h21)
+        max(a,b) == findMin(h)
+    }
+
+  property("break4: find min returns root of first list element in head rank instead of swipe through list") =
+    forAll { (a : A, b: A) =>
+      val hMin = insert(min(a,b), empty)
+      val hMax2 = insert(max(a,b), empty)
+      val hMixedR1 = meld(hMin, hMax2)
+
+      val hMaxR0 = insert(max(a,b), empty)
+
+      max(a,b) == findMin(deleteMin(meld(hMaxR0, hMixedR1)))
     }
 }
