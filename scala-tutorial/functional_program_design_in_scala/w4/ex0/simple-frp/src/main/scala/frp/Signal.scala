1a710021c3e7562d25842f534a24f32682fc9324
Joël Vallone <joel.vallone@gmail.com>
2019-03-23 09:07:24 +0100

    Add signals as simple FRP implementation

---
+++ scala-tutorial/functional_program_design_in_scala/w4/ex0/simple-frp/src/main/scala/frp/Signal.scala
@@ -0,0 +1,39 @@
+package frp
+
+class Signal[T](expr: => T) {
+  import Signal._
+  private var myExpr: () => T = _
+  private var myValue: T = _
+  private var observers: Set[Signal[_]] = Set()
+  update(expr)
+
+  protected def update(expr: => T): Unit = {
+    myExpr = () => expr
+    computeValue()
+  }
+
+  protected def computeValue(): Unit = {
+    myValue = caller.withValue(this)(myExpr())
+  }
+
+  def apply() = {
+    observers += caller.value
+    assert(!caller.value.observers.contains(this), "cyclic signal definition")
+    myValue
+  }
+}
+
+object Signal {
+
+  val caller = new StackableVariable[Signal[_]](NoSignal)
+  def apply[T](expr: => T) = new Signal(expr)
+
+  class StackableVariable[T](init: T) {
+    private var values: List[T] = List(init)
+    def value: T = values.head
+    def withValue[R](newValue: T)(op: => R): R = {
+      values = newValue :: values
+      try op finally values = values.tail
+    }
+  }
+}
