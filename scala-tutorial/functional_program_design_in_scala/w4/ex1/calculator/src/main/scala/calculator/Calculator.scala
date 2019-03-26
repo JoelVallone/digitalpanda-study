d71caee1da0cc7f0caa834fdb9850d168d9c506d
Joël Vallone <joel.vallone@swisscom.com>
2019-03-26 07:24:13 +0100

    Compute values (no cycle detection)

--- scala-tutorial/functional_program_design_in_scala/w4/ex1/calculator/src/main/scala/calculator/Calculator.scala
+++ scala-tutorial/functional_program_design_in_scala/w4/ex1/calculator/src/main/scala/calculator/Calculator.scala
@@ -10,19 +10,26 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
   def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
-  }
+      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] =
+    namedExpressions.map{
+      case (name, expr) => (name, Signal{ eval(expr(), namedExpressions)})}
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+  // TODO: Detect Ref cycles
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double =  expr match {
+    case Literal(v)   => v
+    case Ref(name)    => eval(getReferenceExpr(name, references), references)
+    case Plus(a, b)   => eval(a, references) + eval(b, references)
+    case Minus(a, b)  => eval(a, references) - eval(b, references)
+    case Times(a, b)  => eval(a, references) * eval(b, references)
+    case Divide(a, b) => eval(a, references) / eval(b, references)
+    case _  => Double.NaN
   }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
   private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
+      references: Map[String, Signal[Expr]]): Expr = {
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
