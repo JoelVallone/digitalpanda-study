c80b4edf96e2c77660db3aea57353d995c556fd7
Joël Vallone <joel.vallone@swisscom.com>
2019-05-29 07:50:45 +0200

    WIP: observatory.Extraction

--- scala-tutorial/c5_scala_capstone/observatory/src/main/scala/observatory/Extraction.scala
+++ scala-tutorial/c5_scala_capstone/observatory/src/main/scala/observatory/Extraction.scala
@@ -2,6 +2,9 @@ package observatory
 
 import java.time.LocalDate
 
+import observatory.Main.sc
+import org.apache.spark.rdd.RDD
+
 /**
   * 1st milestone: data extraction
   */
@@ -14,6 +17,12 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
+    sparkLocateTemperatures(year, stationsFile, temperaturesFile).collect().seq
+  }
+
+  def sparkLocateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): RDD[(LocalDate, Location, Temperature)] = {
+    val tempLines: RDD[String]  = sc.textFile(temperaturesFile)
+    val stationLines: RDD[String]  = sc.textFile(stationsFile)
     ???
   }
 
@@ -22,7 +31,22 @@ object Extraction {
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    ???
+    sparkAverageRecords(sc.parallelize(records)).collect().toSeq
+  }
+
+  def sparkAverageRecords(records: RDD[(LocalDate, Location, Temperature)]): RDD[(Location, Temperature)] = {
+     records
+        .map(t => (t._2, t._3))
+        .groupBy(_._1)
+        .mapValues(
+          measures => {
+            val (sum, count) = measures.par
+              .map(t => (t._2, 1))
+              .fold(0.0, 0)((t1, t2) => (t1._1 + t2._1, t1._2 + t2._2))
+              if (count != 0) sum / count else 0
+          }
+        )
+
   }
 
 }
--- scala-tutorial/c5_scala_capstone/observatory/src/main/scala/observatory/Main.scala
+++ scala-tutorial/c5_scala_capstone/observatory/src/main/scala/observatory/Main.scala
@@ -1,6 +1,16 @@
 package observatory
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 object Main extends App {
+
+  @transient lazy val conf: SparkConf = new SparkConf()
+  .setMaster("local")
+  .setAppName("StackOverflow")
+  .set("spark.driver.bindAddress", "127.0.0.1")
+
+  @transient lazy val sc: SparkContext = new SparkContext(conf)
+
 
 
 }
