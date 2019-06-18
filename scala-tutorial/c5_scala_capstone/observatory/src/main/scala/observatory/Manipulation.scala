4ffdbcf7b2d58cfac27f95ba3aeda29b7e98bdba
Joël Vallone <joel.vallone@swisscom.com>
2019-06-18 07:27:12 +0200

    WIP: observatory.Manipulation

--- scala-tutorial/c5_scala_capstone/observatory/src/main/scala/observatory/Manipulation.scala
+++ scala-tutorial/c5_scala_capstone/observatory/src/main/scala/observatory/Manipulation.scala
@@ -1,9 +1,13 @@
 package observatory
 
+import observatory.Visualization.predictTemperature
+
+
 /**
   * 4th milestone: value-added information
   */
 object Manipulation {
+
 
   /**
     * @param temperatures Known temperatures
@@ -11,7 +15,14 @@ object Manipulation {
     *         returns the predicted temperature at this location
     */
   def makeGrid(temperatures: Iterable[(Location, Temperature)]): GridLocation => Temperature = {
-    ???
+    makeGridEager(temperatures)
+  }
+
+  def makeGridEager(temperatures: Iterable[(Location, Temperature)]): GridLocation => Temperature = {
+    val gridTemp = earthGrid().par
+      .map(gLocation => gLocation -> predictTemperature(temperatures, gLocation.location))
+      .toMap
+    gLocation: GridLocation => gridTemp(gLocation)
   }
 
   /**
@@ -20,7 +31,16 @@ object Manipulation {
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
   def average(temperaturess: Iterable[Iterable[(Location, Temperature)]]): GridLocation => Temperature = {
-    ???
+    averageEager(temperaturess)
+  }
+
+  def averageEager(temperaturess: Iterable[Iterable[(Location, Temperature)]]): GridLocation => Temperature = {
+    val tempGrids = temperaturess.map(temperatures => makeGrid(temperatures))
+    val numYears = temperaturess.size
+    val gridAvgTemp = earthGrid().par
+      .map( gLocation => gLocation -> tempGrids.map(_(gLocation)).sum / numYears)
+      .toMap
+    gLocation: GridLocation => gridAvgTemp(gLocation)
   }
 
   /**
@@ -29,9 +49,22 @@ object Manipulation {
     * @return A grid containing the deviations compared to the normal temperatures
     */
   def deviation(temperatures: Iterable[(Location, Temperature)], normals: GridLocation => Temperature): GridLocation => Temperature = {
-    ???
+    deviationEager(temperatures, normals)
   }
 
+  def deviationEager(temperatures: Iterable[(Location, Temperature)], normals: GridLocation => Temperature): GridLocation => Temperature = {
+    val tempGrid = makeGrid(temperatures)
+    val gridDevTemp = earthGrid()
+      .map( gLocation => gLocation -> (tempGrid(gLocation) - normals(gLocation)))
+      .toMap
+    gLocation: GridLocation => gridDevTemp(gLocation)
+  }
+
+  private def earthGrid(): Iterable[GridLocation] =
+    for {
+      lat <- -89 to 90
+      lon <- -180 to 179
+    } yield GridLocation(lat, lon)
 
 }
 
