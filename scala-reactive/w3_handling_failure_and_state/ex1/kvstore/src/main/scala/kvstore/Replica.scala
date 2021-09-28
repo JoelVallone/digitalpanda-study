cbfabe304025d071fa8596558fa0403fee6142b9
Joël Vallone <joel.vallone@swisscom.com>
2021-09-28 08:00:56 +0200

    scala-reactive : WIP kv store primary persitence & replication

--- scala-reactive/w3_handling_failure_and_state/ex1/kvstore/src/main/scala/kvstore/Replica.scala
+++ scala-reactive/w3_handling_failure_and_state/ex1/kvstore/src/main/scala/kvstore/Replica.scala
@@ -30,18 +30,8 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor:
   import Replicator.*
   import Persistence.*
   import context.dispatcher
-
-  /*
-   * The contents of this actor is just a suggestion, you can implement it in any way you like.
-   */
   
   var kv = Map.empty[String, String]
-  // a map from secondary replicas to replicators
-  var secondaries = Map.empty[ActorRef, ActorRef]
-  // the current set of replicators
-  var replicators = Set.empty[ActorRef]
-
-  val persistence = context.actorOf(persistenceProps)
 
   override def preStart(): Unit = {
     super.preStart()
@@ -49,69 +39,121 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor:
   }
 
   def receive =
-    case JoinedPrimary   => context.become(leader)
+    case JoinedPrimary   => {
+      context.become(leader)
+      secondaries += (self, context.actorOf(Replicator.props(self)))
+      replicators += self
+    }
     case JoinedSecondary => context.become(replica)
 
-  /* TODO Behavior for  the leader role. */
+
+
+
+  // Behavior for the LEADER role.
+  var secondaries = Map.empty[ActorRef, ActorRef] // (Replica -> Replicator)
+  var replicators = Set.empty[ActorRef]
+  var inFlightReplications = Map.empty[Long, (ActorRef, Set[ActorRef])] // (Operation.id -> (operation sender, Set(Replicator)))
+
+  //TODO: test+fix primary persistence & replication
   val leader: Receive = {
     case Insert(key, value, id) => {
-      kv = kv + (key -> value)
-      sender ! OperationAck(id)
+      kv += (key -> value)
+      //sender ! OperationAck(id)
+      // // forwardStore(Persist(key, Some(value), id))
+      replicators.foreach( r =>  r.forward(Replicate(key, Some(value), id)))
+      inFlightReplications += (id, (sender(), replicators))
     }
     case Remove(key, id) => {
-      kv = kv - key
-      sender ! OperationAck(id)
+      kv -= key
+      //sender ! OperationAck(id)
+      // // forwardStore(Persist(key, None, id))
+      replicators.foreach( r =>  r.forward(Replicate(key, None, id)))
+      inFlightReplications += (id, (sender(), replicators))
     }
+
+    // TODO: check write ordering: last write request wins... Replicator.__seqCounter should do the trick
+    case Replicated(key, id) => {
+      val inFlightReplicationUpdate = (inFlightReplications(id)._1, inFlightReplications(id)._2 - sender())
+      if (inFlightReplicationUpdate._2.isEmpty) {
+        inFlightReplicationUpdate._1 ! OperationAck(id)
+        inFlightReplications -= id
+      } else {
+        inFlightReplications += (id, inFlightReplicationUpdate)
+      }
+    }
+    /*
+    case Get(key, id) => {
+      sender ! GetResult(key, kv.get(key), id)
+    }*/
+    case Replicas(newReplicas: Set[ActorRef]) => {
+      //Update tracking structures for join and leave
+      val addedReplicas = newReplicas -- secondaries.keySet
+      val newSecondaries = addedReplicas.map(newReplica => (newReplica, context.actorOf(Replicator.props(newReplica)))).toMap
+      secondaries ++= newSecondaries
+      replicators ++= newSecondaries.values.toSet
+
+      val removedSecondaries = secondaries -- newReplicas
+      removedSecondaries.foreach{ (_, replicator) => context.stop(replicator)}
+      secondaries --= removedSecondaries.keySet
+      replicators --= removedSecondaries.values.toSet
+
+
+      //TODO: Handle transmission of initial state to new replica
+    }
+    case message => replica(message) // A leader also behaves as a replica (persistence and get)
+  }
+
+
+
+
+  // Behavior for the REPLICA role.
+  val persistence = context.actorOf(persistenceProps) // supervisorStrategy defaults to always restart failing child
+  var inFlightPersist = Map.empty[String, (ActorRef, Persist)]
+  var expectedSeq = 0L
+
+  val replica: Receive = {
     case Get(key, id) => {
       sender ! GetResult(key, kv.get(key), id)
     }
-    case _ =>
+
+    case message => persistenceHandler(message)
   }
 
-  
-  var expectedSeq = 0L
-  
-  /* TODO Behavior for the replica role. */
-  val replica: Receive = {
+  val persistenceHandler: Receive = {
     case s: Snapshot => {
       if (s.seq < expectedSeq) {
         sender ! SnapshotAck(s.key, s.seq)
       } else if (s.seq == expectedSeq) {
-        scheduleStore(sender, Persist(s.key, s.valueOption, s.seq))
         expectedSeq = max(expectedSeq, s.seq + 1)
-        s.valueOption match {
-          case None => kv = kv - s.key
-          case Some(value) => kv = kv + (s.key -> value)
-        }
+        forwardStore(Persist(s.key, s.valueOption, s.seq))
       }
     }
-
     case p: Persisted => {
       val (sender, persist) = inFlightPersist(p.key)
       val seq = persist.id
       sender ! SnapshotAck(persist.key, seq)
-      inFlightPersist = inFlightPersist - p.key
+      inFlightPersist -= p.key
     }
-
-    case ResendStore(key) => {
-      inFlightPersist.get(key) match {
-        case Some((sender, persist)) => scheduleStore(sender, persist)
+    case r: ResendStore => {
+      inFlightPersist.get(r.key) match {
+        case Some((sender, persist)) => scheduleStoreRetry(sender, persist)
         case None => //nil
       }
     }
-
-    case Get(key, id) => {
-      sender ! GetResult(key, kv.get(key), id)
-    }
-
     case _ =>
   }
 
-  var inFlightPersist = Map.empty[String, (ActorRef, Persist)]
+  def forwardStore(p: Persist): Unit = {
+    scheduleStoreRetry(sender, p)
+    p.valueOption match {
+      case None => kv -= p.key
+      case Some(value) => kv += (p.key -> value)
+    }
+  }
 
-  def scheduleStore(sender: ActorRef, p: Persist): Unit = {
+  def scheduleStoreRetry(sender: ActorRef, p: Persist): Unit = {
     persistence ! p
-    inFlightPersist = inFlightPersist + (p.key -> (sender,p))
+    inFlightPersist += (p.key -> (sender,p))
     context.system.scheduler.scheduleOnce(100.milliseconds, self, ResendStore(p.key))
   }
 
